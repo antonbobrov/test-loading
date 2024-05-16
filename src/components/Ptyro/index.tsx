6303fdc9e20f0416aba1f2bc37b0ee3a35a13f2e
Anthony Bobrov <anton.bobrov@hotmail.com>
2024-05-16 22:22:32 +0400

    Test

---
+++ src/components/Ptyro/index.tsx
@@ -0,0 +1,14 @@
+import { FC, useState } from 'react';
+
+import Sequence from './SEQUENCE.json';
+
+export const Ptyro = () => {
+
+  return (
+    <>
+      {Sequence.map((item) => {
+        return <img key={item.webp} src={'/ptyro/' + item.webp} />
+      })}
+    </>
+  );
+};
